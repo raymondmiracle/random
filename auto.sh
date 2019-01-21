6e67ada5bda43d5ad7e0fd2161331ea16b3dac2b
raymondmiracle <34572535+raymondmiracle@users.noreply.github.com>
2019-01-21 23:08:55 +0100

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,7 +1,7 @@
 git clone https://github.com/raymondmiracle/Toolchain /Toolchain
 git clone https://github.com/raymondmiracle/anykernel
 export ARCH=arm64
-export CROSS_COMPILE=~/Toolchain/bin/aarch64-linux-android-
+export CROSS_COMPILE=/Toolchain/bin/aarch64-linux-android-
 mkdir output
 make -C $(pwd) O=output k606kd_defconfig
 make -j32 -C $(pwd) O=output
