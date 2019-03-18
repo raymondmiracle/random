09bfb165b4aea6210b8d2513530ff1b3d413f2c4
raymondmiracle <34572535+raymondmiracle@users.noreply.github.com>
2019-03-18 13:52:12 -0700

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,11 +1,11 @@
-git clone https://github.com/raymondmiracle/Toolchain /Toolchain
+git clone https://github.com/raymondmiracle/Toolchain /toolchain
 git clone https://github.com/raymondmiracle/anykernel
 export ARCH=arm64
-export CROSS_COMPILE=/Toolchain/bin/aarch64-linux-android-
+export CROSS_COMPILE=$pwd/toolchain/bin/aarch64-linux-android-
 mkdir output
 make -C $(pwd) O=output k606kd_defconfig
 make -j32 -C $(pwd) O=output
 cp -r output/arch/arm64/boot/Image.gz-dtb anykernel/
 cd anykernel
 mv Image.gz-dtb zImage 
-zip -r KERNEL-V1.1.zip *
+zip -r9 kernel *
